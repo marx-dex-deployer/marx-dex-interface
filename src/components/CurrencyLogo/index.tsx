2d9cc4a45dc8e36ad6a3d02ec0907690a52f0967
MarX Decentralized Exchange Deployer <96611603+marx-dex-deployer@users.noreply.github.com>
2022-01-25 11:12:52 +0200

    Update tokens logo

--- src/components/CurrencyLogo/index.tsx
+++ src/components/CurrencyLogo/index.tsx
@@ -2,13 +2,13 @@ import { Currency, ETHER, Token } from '@marx-dex/marx-dex-v2-sdk'
 import React, { useMemo } from 'react'
 import styled from 'styled-components'
 
-import EthereumLogo from '../../assets/images/ethereum-logo.png'
+import EthereumLogo from '../../assets/images/evmos.png'
 import useHttpLocations from '../../hooks/useHttpLocations'
 import { WrappedTokenInfo } from '../../state/lists/hooks'
 import Logo from '../Logo'
 
 const getTokenLogoURL = (address: string) =>
-  `https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/${address}/logo.png`
+  `https://raw.githubusercontent.com/marx-dex-deployer/marx-dex-interface/main/src/assets/images/tokens/${address}.png`
 
 const StyledEthereumLogo = styled.img<{ size: string }>`
   width: ${({ size }) => size};
